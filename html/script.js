24faf8ea1f7303091db2a596e293cbd9fd964372
Joe Koop <joe@joekoop.com>
2025-10-13 21:19:12 -0500

    Prevent non-left mouse button clicks from triggering anchor navigation in script.js

--- html/script.js
+++ html/script.js
@@ -1,6 +1,7 @@
 // eager anchors
 window.addEventListener("mousedown", event => {
+    if (event.button != 0) return;
     const anchor = event.target.closest("a");
     if (anchor == null) return;
-    location.href = anchor.href;
+    anchor.click();
 });
